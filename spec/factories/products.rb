b7bbf63b13c8c1c90d19c07d0a7ca2f8b792c4bf
designerofthing <designerofthings@gmail.com>
2020-05-05 11:49:28 +0200

    adds product model spec, generates product model, migrates db

---
+++ spec/factories/products.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :product do
+    title { "MyString" }
+    description { "MyString" }
+    category { "" }
+    price { 1 }
+    image_url { "MyString" }
+    tag { "MyString" }
+  end
+end
