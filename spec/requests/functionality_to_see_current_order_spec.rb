29552805ae0c1455ffa2b8ad629b32faa750c3da
pauline_barnades <barnades.pauline@gmail.com>
2020-05-07 11:30:05 +0200

    Finishing the functionailty test, removing and addin new order controllers with correct name, adding order modules and order_items modules

--- spec/requests/functionality_to_see_current_order_spec.rb
+++ spec/requests/functionality_to_see_current_order_spec.rb
@@ -1,10 +1,8 @@
-RSpec.describe Api::OrdersController, type: :request do 
+RSpec.describe Api::V1::OrdersController, type: :request do 
 let!(:product_1) {create(:product, title: 'Pizza', id: 1)}
 let!(:product_2) {create(:product, title: 'Hamburger', id: 2)}
-it "successfully" do 
-post '/api/v1/orders', params: {id:product_1}
-expect(JSON.parse(response.body)['message']).to eq 'The product has been added to your order'
-
-end
-
+  it "successfully" do 
+    post '/api/v1/orders', params: {id:product_1}
+    expect(JSON.parse(response.body)['message']).to eq 'The product has been added to your order'
+  end
 end
