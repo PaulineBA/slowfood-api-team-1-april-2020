accae572072e85de3a559eabf46385a2af65843e
pauline_barnades <barnades.pauline@gmail.com>
2020-05-07 12:00:41 +0200

    testing relations and database table for order item

--- spec/requests/api/v1/user_can_have_order_items_in_his_order_spec.rb
+++ spec/requests/api/v1/user_can_have_order_items_in_his_order_spec.rb
@@ -3,3 +3,13 @@ RSpec.describe Order, type: :model do
     it { is_expected.to have_many :order_items }
   end
 end
+RSpec.describe OrderItem, type: :model do 
+  describe 'Database table' do
+    it { is_expected.to have_db_column :order_id }
+    it { is_expected.to have_db_column :product_id }
+  end
+  describe 'Relations' do
+    it { is_expected.to belong_to :product }
+    it { is_expected.to belong_to :order }
+  end
+end
