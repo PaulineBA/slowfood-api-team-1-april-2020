4e109b013b3852181d7595320917ae7f77f69632
japh <jenny.scherr@svenskaspel.se>
2020-05-08 10:08:09 +0200

    added test to the order request spec

--- spec/requests/api/v1/functionality_to_see_current_order_spec.rb
+++ spec/requests/api/v1/functionality_to_see_current_order_spec.rb
@@ -1,8 +1,26 @@
 RSpec.describe Api::V1::OrdersController, type: :request do 
-let!(:product_1) {create(:product, title: 'Pizza', id: 1)}
-let!(:product_2) {create(:product, title: 'Hamburger', id: 2)}
-  it "successfully" do 
-    post '/api/v1/orders', params: {id:product_1}
-    expect(JSON.parse(response.body)['message']).to eq 'The product has been added to your order'
+  let!(:product_1) {create(:product, title: 'Pizza', id: 1)}
+  let!(:product_2) {create(:product, title: 'Hamburger', id: 2)}
+  before do
+    post '/api/v1/orders', 
+    params: {
+      id:product_1,
+    }
+    @order_id = JSON.parse(response.body)["order_id"]
+  end
+  
+  describe 'POST /order' do
+    it "successfully" do       
+      expect(JSON.parse(response.body)['message']).to eq 'The product has been added to your order'
+    end
+
+    it 'should return a 200 response' do
+      expect(response).to have_http_status 200
+    end
+
+    it "should return order id in response" do
+      order = Order.find(@order_id)
+      expect(JSON.parse(response.body)["order_id"]).to eq order.id
+    end
   end
 end
