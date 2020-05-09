af7f5c81ed08e9c5f04b62df4c008f70fb1e1c7c
pauline_barnades <barnades.pauline@gmail.com>
2020-05-09 12:17:34 +0200

    testing adds products and the response from the functionality spec

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :product do
-    id { 2 }
     title { "Pizza" }
     description { "Best pizza in town" }
     price { 110 }
--- spec/requests/api/v1/functionality_to_see_current_order_spec.rb
+++ spec/requests/api/v1/functionality_to_see_current_order_spec.rb
@@ -1,18 +1,18 @@
 RSpec.describe Api::V1::OrdersController, type: :request do 
-  let!(:product_1) {create(:product, title: 'Pizza', id: 1)}
-  let!(:product_2) {create(:product, title: 'Hamburger', id: 2)}
+  let!(:product_1) {create(:product, title: 'Pizza')}
+  let!(:product_2) {create(:product, title: 'Hamburger')}
 
   before do
     post '/api/v1/orders/', 
     params: {
       product_id: product_1.id
     }
-    @order_id = JSON.parse(response.body)["order_id"]
+    @order_id = response_json["order_id"]
   end
   
-  describe 'POST api/order' do
+  describe 'POST /api/v1/order' do
     it "successfully" do       
-      expect(JSON.parse(response.body)['message']).to eq 'The product has been added to your order'
+      expect(response_json['message']).to eq 'The product has been added to your order'
     end
     
     it 'should return a 200 response' do
@@ -21,22 +21,22 @@ RSpec.describe Api::V1::OrdersController, type: :request do
 
     it "should return order id in response" do
       order = Order.find(@order_id)
-      expect(JSON.parse(response.body)["order_id"]).to eq order.id
+      expect(response_json["order_id"]).to eq order.id
     end
   end
 
-  describe 'PUT /api/orders/:id' do
+  describe 'PUT /api/v1/orders/:id' do
     before do
-      put "api/orders/#{@order_id}", params: { product_id: product_2.id }
+      put "/api/v1/orders/#{@order_id}", params: { product_id: product_2.id }
       @order = Order.find(@order_id)
     end
 
     it "adds another product to the order" do
-      expect(@order_items.count).to_eq 2
+      expect(@order.order_items.count).to eq 2
     end
 
     it "responds with order id" do
-      expect(JSON.parse(response.body)["order_id"]).to eq @order.id
+      expect(response_json["order_id"]).to eq @order.id
     end
   end
 end
