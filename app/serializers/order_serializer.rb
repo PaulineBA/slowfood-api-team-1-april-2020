0f214a692005fa3f49ec0fcb9fbca7d238d2f33a
Erik Björn <erik.g.bjoern@gmail.com>
2020-05-11 21:33:48 +0200

    removing total method from serializers, since we don't use it

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,14 +1,10 @@
 class OrderSerializer < ActiveModel::Serializer
-  attributes :id, :products, :total, :order_total
+  attributes :id, :products, :order_total
 
   def products
     object.order_items.group_by(&:product_id).map do |_key, value|
       product = value.uniq(&:product_id)[0].product
       { amount: value.size, title: product.title, price: (value.size * product.price), product_id: product.id }
     end
-  end
-
-  def total
-    object.order_items.joins(:product).sum('products.price')
   end
 end
