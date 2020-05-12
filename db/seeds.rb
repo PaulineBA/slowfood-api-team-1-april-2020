5af58cef67d0301c7fd70573215b05065b0075f7
pauline_barnades <barnades.pauline@gmail.com>
2020-05-12 09:38:40 +0200

    populating db in heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+products = Product.create(
+[{ title: "pizza", "description": "best pizza in town","price": 10},
+{"title": "hamburger","description": "everything on it","price": 6},
+{"title": "kebab","description": "kebab in pita bread","price": 7}])
+    
